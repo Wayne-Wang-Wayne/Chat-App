29585224c69434229ff29a78d88efe7c5d0f2be2
Wayne-Wang-Wayne <w702897028970289@gmail.com>
2022-02-13 11:56:07 +0800

    initial commit

---
+++ app/src/main/java/com/example/chatapp/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.chatapp.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chatapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
