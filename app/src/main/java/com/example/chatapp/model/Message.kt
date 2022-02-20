326e6c4d86d08c6595b0ddaeb1afaec0599ff6fe
Wayne-Wang-Wayne <w702897028970289@gmail.com>
2022-02-20 19:45:18 +0800

    修正pro guard 錯誤

--- app/src/main/java/com/example/chatapp/model/Message.kt
+++ app/src/main/java/com/example/chatapp/model/Message.kt
@@ -1,5 +1,8 @@
 package com.example.chatapp.model
 
+import androidx.annotation.Keep
+
+@Keep
 data class Message(
     var message: String? = "",
     var senderUId: String? = ""
--- app/src/main/java/com/example/chatapp/model/User.kt
+++ app/src/main/java/com/example/chatapp/model/User.kt
@@ -1,5 +1,8 @@
 package com.example.chatapp.model
 
+import androidx.annotation.Keep
+
+@Keep
 data class User(
     var name: String? = "",
     var email: String? = "",
